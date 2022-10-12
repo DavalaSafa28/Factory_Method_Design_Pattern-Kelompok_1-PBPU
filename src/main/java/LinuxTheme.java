19af2324264f1f9d021b1b6c3b4e47850d423a79
danieldeary15 <89431863+danieldeary15@users.noreply.github.com>
2022-10-12 15:14:30 +0700

    Create LinuxTheme.java

---
+++ src/main/java/LinuxTheme.java
@@ -0,0 +1,10 @@
+public class LinuxTheme implements Theme {
+    public void print() {
+        System.out.println("Ini Tampilan Tema Linux");
+        onClick();
+    }
+    
+    public void onClick() {
+        System.out.println("Tema Linux");
+    }
+}
