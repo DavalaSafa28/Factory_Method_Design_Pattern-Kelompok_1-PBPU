3ae97617da937019b7c740b264b0446558320c8f
DavalaSafa28 <286dava@gmail.com>
2022-10-12 15:01:51 +0700

    Create IOSWallpaper.java

---
+++ src/main/java/IOSWallpaper.java
@@ -0,0 +1,11 @@
+public class IOSWallpaper implements Wallpaper {
+    
+    public void print() {
+        System.out.println("Ini Wallpaper IOS");
+        onClick();
+    }
+    
+    public void onClick() {
+        System.out.println("Wallpaper IOS");
+    }
+}
