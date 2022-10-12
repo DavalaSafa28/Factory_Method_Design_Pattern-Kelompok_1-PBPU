ecd761c44bc88ae6c344027b6c98b83e51696362
danieldeary15 <89431863+danieldeary15@users.noreply.github.com>
2022-10-12 15:15:27 +0700

    Create LinuxWallpaper.java

---
+++ src/main/java/LinuxWallpaper.java
@@ -0,0 +1,10 @@
+public class LinuxWallpaper implements Wallpaper {
+    public void print() {
+        System.out.println("Ini Wallpaper Linux");
+        onClick();
+    }
+    
+    public void onClick() {
+        System.out.println("Wallpaper Linux");
+    }
+}
